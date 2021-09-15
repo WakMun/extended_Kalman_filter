b13a7c26c406e94706f7de88f402fb77cda3f3b4
wakmun <wakmun@vm>
2021-09-15 05:13:59 -0700

    completed laser part

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -152,7 +152,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     DEBUGOUT("Updating on base of laser data.");
     ekf_.H_ = H_laser_;
   	ekf_.R_ = R_laser_;
-    ekf_.Update(measurement_pack.raw_measurements_);
+    //ekf_.Update(measurement_pack.raw_measurements_);
 
   }
 
